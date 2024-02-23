675e6a8b90c70cbd6bc1352ba320f5ff96ba72ef
ritwic <rtvkraj16@gmail.com>
2024-02-23 22:36:34 +0530

    test-2

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
 import Login from './src/screens/Login'
 
 export default function App() {
+  let [name,setName]=useState('Ritwik')
+  let [age,setAge] =useState(28)
   return (
     <View>
       <Login/>
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,40 @@
-import { View, Text } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Login() {
+export default function login() {
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.v}>
+        <Text style={styles.title}>Rythm</Text>
+        <Text style={styles.title}>Run</Text>
+      </View>
+      
+      <View style={{position:'absolute', width:'140%',top:'-30%'}}>
+        <Image style={{width:'100%',height: undefined,aspectRatio:1}} source={require('../assets/grad.png')}/>
+      </View>
+
+      <View style={{position:'absolute', width:'70%', bottom:'-10%',left:"-30%"}}>
+        <Image style={{width:'100%',height: undefined,aspectRatio:1}} source={require('../assets/grad.png')}/>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    width: "100%",
+    height: "100%",
+    backgroundColor: "black",
+  },
+  v:{
+    flex:1,
+    zIndex:2,
+    padding:30,
+    alignItems:'flex-end'
+  },
+  title:{
+    color:'white',
+    fontSize:50,
+    lineHeight:50
+  }
+})
